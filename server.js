076b29d8266c15f37460011bdab694c5869f103a
harili <ilieschafyfy@gmail.com>
2020-08-28 19:16:54 +0200

    Ajout des dépendances - Correction de la faute .env - connexion réussite - ajout de nodemon pour éviter les restarts

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+/**Configuration du server Express */
+
+const express = require("express"); //une nuvelle instance d'express
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 //Initialisation des variables ^pur créer l'app
 const app = express();
@@ -9,12 +11,15 @@ const port = process.env.PORT || 8080;
 app.use(cors()); //Connexion côté cline tavec le côté server
 app.use(express.json()); //pas besoin du body parser
 
+require("dotenv").config();
+
 /*Connexion du MongoDB*/
  const uri = process.env.CONNEXION_BLOG; //pour la connex. au .env file
 
  
  mongoose.connect(uri, {
-     useNewUrlParser: true // Tant que c'est vrai
+     useNewUrlParser: true, // Tant que c'est vrai
+     useUnifiedTopology: true
  });
 
  const connexion = mongoose.connection; //Si la connexion est ouverte une première fois
@@ -22,3 +27,5 @@ app.use(express.json()); //pas besoin du body parser
 connexion.once("open", ()=>
  console.log("La connexion avec la BDD de MongoDB a bien été effectué") //Okay
 );
+
+app.listen(port, () => console.log(`l'application est déployé sur le port suivant : ${port}`));
